485abc3b4d18b7f760ba7c7c8ce17b996a86e038
hejo47 <hejo_47@naver.com>
2024-10-14 09:42:44 +0900

    feat: intro 작업(basicInfo 연결 중)_241014

--- src/components/invitationSection/Intro.js
+++ src/components/invitationSection/Intro.js
@@ -1,20 +1,17 @@
 /* Import */
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 /* Component */
 /* CSS Module */
 import styles from "../../css/module/invitationSection/Intro.module.scss";
 /* Image */
 import introRing from "../../img/intro/intro_theme_1_ring.png";
+/* Context */
+import { InfoContext } from "../../store/option-info-context.js";
 
 const Intro = () => {
-  // useEffect(() => {
-  //   setIsActive(0);
-  // }, []);
-  
+  const { basicInfoList, setBasicInfoList } = useContext(InfoContext);
   return (
-    <div 
-      className={`${styles.intro} ${styles.style_theme_1}`}
-    >
+    <div className={`${styles.intro} ${styles.style_theme_1}`}>
       <div className={styles.intro__wrap}>
         <div className={styles.intro__headline}>
           <div className={styles.img__wrap}>
@@ -25,8 +22,22 @@ const Intro = () => {
             <p>Invitation</p>
           </div>
         </div>
-        <div className={styles.intro__photo}></div>
-        <div className={styles.intro__title}></div>
+        <div className={styles.intro__photo}>
+          <img src="" alt="" />
+        </div>
+        <div className={styles.intro__title}>
+          <div className={styles.intro__title_couple}>
+            <h2 className={styles.groom}>{basicInfoList.groomInfo.lastname + basicInfoList.groomInfo.firstname}</h2>
+            <p className={styles.divide}></p>
+            <h2 className={styles.bride}>{basicInfoList.brideInfo.lastname + basicInfoList.brideInfo.firstname}</h2>
+          </div>
+          <div className={styles.intro__title_date}>
+            <h3 className={styles.date}>{`${basicInfoList.dateInfo.date} 오전 ${basicInfoList.timeInfo.hour}:${basicInfoList.timeInfo.min}`}</h3>
+          </div>
+          <div className={styles.intro__title_place}>
+            <h3 className={styles.groom}>{`${basicInfoList.placeInfo.placeName} ${basicInfoList.placeInfo.placeDetail}`}</h3>
+          </div>
+        </div>
       </div>
     </div>
   )
