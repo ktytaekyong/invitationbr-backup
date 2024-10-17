614ce61e3145d813e27f74426606e57285042f2e
hejo47 <hejo_47@naver.com>
2024-10-17 13:33:07 +0900

    feat: D-Day_241017

--- src/components/common/DateSetting.js
+++ src/components/common/DateSetting.js
@@ -1,3 +1,5 @@
+/* Import */
+import { useState, useEffect, useContext } from "react";
 /* Component */
 import CommonOptionWrapper from "./CommonOptionWrapper.js";
 import CommonOptionContent from "./CommonOptionContent.js";
@@ -6,25 +8,61 @@ import CommonItemContent from "./CommonItemContent.js";
 import OptionSelector from "./OptionSelector.js";
 import DateSettingImage from "./DateSettingImage.js";
 import DateSettingCheckbox from "./DateSettingCheckbox.js";
+import BasicInfomationSettingDate from "./BasicInfomationSettingDate.js";
 /* CSS Module */
+/* Context */
+import { InfoContext } from "../../store/option-info-context.js";
 
-const dateHourDistList = ["오전", "오후"];
-const dateHourList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const dateMinuteList = [0, 10, 20, 30, 40, 50];
+const dateHourList = [
+  {itemKey: 8, itemName: "오전 8시"}, {itemKey: 9, itemName: "오전 9시"}, {itemKey: 10, itemName: "오전 10시"}, {itemKey: 11, itemName: "오전 11시"}, {itemKey: 12, itemName: "오후 12시"}, {itemKey: 13, itemName: "오후 1시"}, {itemKey: 14, itemName: "오후 2시"}, {itemKey: 15, itemName: "오후 3시"}, {itemKey: 16, itemName: "오후 4시"}, {itemKey: 17, itemName: "오후 5시"}, {itemKey: 18, itemName: "오후 6시"}, {itemKey: 19, itemName: "오후 7시"}, {itemKey: 20, itemName: "오후 8시"}, {itemKey: 21, itemName: "오후 9시"}
+];
+const dateMinuteList = [
+  {itemKey: 0, itemName: "정각"}, {itemKey: 10, itemName: "10분"}, {itemKey: 20, itemName: "20분"}, {itemKey: 30, itemName: "30분"}, {itemKey: 40, itemName: "40분"}, {itemKey: 50, itemName: "50분"}
+];
 
 const DateSetting = () => {
+  const { basicInfoList, setBasicInfoList } = useContext(InfoContext);
+  const basicDataChangeHandler = (e, infoType) => {
+    const { name, value } = e.target;
+    setBasicInfoList(prev => ({
+      ...prev,
+      [infoType]: {
+        ...prev[infoType],
+        [name]: value
+      }
+    }))
+  }
   return (
     <CommonOptionWrapper>
       <CommonOptionContent>
 
         <CommonItemWrapper>
           <CommonItemContent title="예식일">
-            <input type="date" name="" id="" />
+            <input 
+              type="date" 
+              id="DateInfoDate" 
+              name="date" 
+              onChange={(e) => basicDataChangeHandler(e, "dateInfo")} 
+              value={basicInfoList.dateInfo.date} 
+            />
           </CommonItemContent>
 
           <CommonItemContent title="예식시간">
-            <OptionSelector selectID="DateHourSelect" listName={dateHourList} type="hour" hour={dateHourDistList}></OptionSelector>
-            <OptionSelector selectID="DateMinuteSelect" listName={dateMinuteList} styleType="minute__selector" type="minute"></OptionSelector>
+            <OptionSelector 
+              selectID="DateInfoHour" 
+              selectName="hour"
+              listName={dateHourList} 
+              value={basicInfoList.timeInfo.hour} 
+              onChange={(e) => basicDataChangeHandler(e, "timeInfo")} 
+            />
+            <OptionSelector 
+              selectID="DateInfoMin" 
+              selectName="min" 
+              listName={dateMinuteList} 
+              value={basicInfoList.timeInfo.min} 
+              onChange={(e) => basicDataChangeHandler(e, "timeInfo")} 
+              styleType={"minute__selector"} 
+            />
           </CommonItemContent>
           
           <CommonItemContent title="이미지" multi={true}>
--- src/components/common/DateSettingImage.js
+++ src/components/common/DateSettingImage.js
@@ -1,8 +1,11 @@
+/* Import */
+import { useState, useEffect } from "react";
 /* Component */
 /* CSS Module */
 import styles from "../../css/module/common/DateSettingImage.module.scss";
 /* Image */
 import CalendarImg from "../../img/calendar/calendar_img.png";
+
 const imageList = [
   {
     src: "",
@@ -19,10 +22,21 @@ const imageList = [
 ]
 
 const DateSettingImage = () => {
+  const [isActive, setIsActive] = useState(0);
+  const setActiveHandler = (idx) => {
+    setIsActive(idx);
+  }
+  useEffect(() => {
+    setIsActive(0);
+  }, []);
   return (
     <ul className={styles.calendar__img_list}>
       {imageList.map((item, idx) => (
-        <li className={styles.calendar__img_item} key={`url(${item.src}${idx}`} style={{backgroundImage: `url(${item.src})`}}>
+        <li 
+          className={`${styles.calendar__img_item} ${isActive === idx ? styles["active"] : ""}`} key={`url(${item.src}${idx}`} 
+          style={{backgroundImage: `url(${item.src})`}}
+          onClick={() => setActiveHandler(idx)}
+        >
         </li>
       ))}
     </ul>
