5d3d05d4c0f1767a863db609fb8f3252d79c0798
hejo47 <hejo_47@naver.com>
2024-10-18 11:43:46 +0900

    feat: 갤러리 (편집) 믹스 작업_241018

--- src/components/invitationSection/GalleryMix.js
+++ src/components/invitationSection/GalleryMix.js
@@ -8,7 +8,7 @@ import 'swiper/css/pagination';
 /* CSS Module */
 import styles from "../../css/module/invitationSection/GalleryMix.module.scss";
 /* Image */
-// import tempMapImg from "../../img/location/temp_map.png";
+import galleryPhoto from "../../img/gallery/slide_photo_test.png";
 /* Context */
 import { GalleryContext } from "../../store/option-gallery-context.js";
 
@@ -26,35 +26,87 @@ const GalleryMix = () => {
         // onSlideChange={() => console.log('slide change')}
         // onSwiper={(swiper) => console.log(swiper)}
       >
+
         {
-          selectGalleryPhotoList.length === 0 ?
-          
+          selectGalleryPhotoList.length !== 0 ?
+          <SwiperSlide>
+            <div className={styles.slide__wrap}>
+              {
+                selectGalleryPhotoList.filter((_, idx) => idx < 5)
+                .map((item, idx) => (
+                  <img src={item.src} alt={item.alt} />
+                ))
+              }
+            </div>
+          </SwiperSlide>
           :
+          <>
+            <SwiperSlide>
+              <div className={styles.slide__wrap}>
+                <img src={galleryPhoto} alt="" />
+                <img src={galleryPhoto} alt="" />
+                <img src={galleryPhoto} alt="" />
+                <img src={galleryPhoto} alt="" />
+                <img src={galleryPhoto} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles.slide__wrap}>
+                <img src={galleryPhoto} alt="" />
+                <img src={galleryPhoto} alt="" />
+                <img src={galleryPhoto} alt="" />
+                <img src={galleryPhoto} alt="" />
+                <img src={galleryPhoto} alt="" />
+              </div>
+            </SwiperSlide>
+          </>
         }
-        {/* <SwiperSlide>
-          <div className={styles.top}>
-            <img src="" alt="" />
-            1
-          </div>
-          <div className={styles.bottom}>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.top}>
-            <img src="" alt="" />
-            21
-          </div>
-          <div className={styles.bottom}>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-          </div>
-        </SwiperSlide> */}
+        {
+          selectGalleryPhotoList.length > 5 ?
+          <SwiperSlide>
+            <div className={styles.slide__wrap}>
+            {
+              selectGalleryPhotoList.filter((_, idx) => (idx > 4) && (idx < 10))
+              .map((item, idx) => (
+                <img src={item.src} alt={item.alt} />
+              ))
+            }
+            </div>
+          </SwiperSlide>
+          :
+          null
+        }
+        {
+          selectGalleryPhotoList.length > 10 ?
+          <SwiperSlide>
+            <div className={styles.slide__wrap}>
+            {
+              selectGalleryPhotoList.filter((_, idx) => (idx > 9) && (idx < 15))
+              .map((item, idx) => (
+                <img src={item.src} alt={item.alt} />
+              ))
+            }
+            </div>
+          </SwiperSlide>
+          :
+          null
+        }
+        {
+          selectGalleryPhotoList.length > 15 ?
+          <SwiperSlide>
+            <div className={styles.slide__wrap}>
+            {
+              selectGalleryPhotoList.filter((_, idx) => (idx > 14) && (idx < 20))
+              .map((item, idx) => (
+                <img src={item.src} alt={item.alt} />
+              ))
+            }
+            </div>
+          </SwiperSlide>
+          :
+          null
+        }
+        
       </Swiper>
     </div>
   )
