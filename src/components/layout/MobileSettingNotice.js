c3c2985e5cb96b24aeb6b4e7200713eb06771b02
hejo47 <hejo_47@naver.com>
2024-11-26 09:45:28 +0900

    feat: 인트로 모바일 알림 수정 _241126

--- src/components/layout/MobileSettingNotice.js
+++ src/components/layout/MobileSettingNotice.js
@@ -14,18 +14,20 @@ const MobileSettingNotice = () => {
   const noticeRenderer = () => {
     if(selectOptionList.theme === "themeModernBasic" 
       && selectOptionList.introFillType === "basicTemplate1" 
-      || selectOptionList.introFillType === "basicTemplate2"
-      || selectOptionList.introFillType === "basicTemplate3"
-      || selectOptionList.introFillType === "basicTemplate4"
+      && selectOptionList.introFillType === "basicTemplate2"
+      && selectOptionList.introFillType === "basicTemplate3"
+      && selectOptionList.introFillType === "basicTemplate4"
     ) {
       return <p className={`${styles.mobile__setting_notice}`}>텍스트, 이미지를 추가하고 편집할 수 있습니다. <br />선택 항목은 체크박스 표시가 있습니다.</p>;
     } else if (selectOptionList.theme === "themeModernBasic" 
       && selectOptionList.introFillType === "fillTemplate1" 
-      || selectOptionList.introFillType === "fillTemplate2"
-      || selectOptionList.introFillType === "fillTemplate3"
-      || selectOptionList.introFillType === "fillTemplate4"
+      && selectOptionList.introFillType === "fillTemplate2"
+      && selectOptionList.introFillType === "fillTemplate3"
+      && selectOptionList.introFillType === "fillTemplate4"
     ) {
       return <p className={`${styles.mobile__setting_notice}`} style={{position: "absolute", top: "350px", left: "50%", width: "80%", transform: "translateX(-50%)", zIndex: "1", borderRadius: "4px", background: "rgba(255, 255, 255, 0.7)", padding: "10px 5px"}}>텍스트, 이미지를 추가하고 편집할 수 있습니다. <br />선택 항목은 체크박스 표시가 있습니다.</p>
+    } else if (selectOptionList.theme === "themeSimple" || selectOptionList.theme === "themeFlower") {
+      return <p className={`${styles.mobile__setting_notice}`} style={{position: "absolute", top: "80px", left: "50%", width: "80%", transform: "translateX(-50%)", zIndex: "1", borderRadius: "4px", background: "rgba(255, 255, 255, 0.7)", padding: "10px 5px"}}>텍스트, 이미지를 추가하고 편집할 수 있습니다. <br />선택 항목은 체크박스 표시가 있습니다.</p>
     }
   }
   return !isTargetPage && isMobile ? noticeRenderer() : null;
