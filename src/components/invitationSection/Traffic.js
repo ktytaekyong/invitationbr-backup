28a21ce254734af5a726d55f79cd57c13fb0a50b
hejo47 <hejo_47@naver.com>
2024-12-11 10:39:51 +0900

    feat: 교통수단 예시 지하철 추가_241211

--- src/components/invitationSection/Traffic.js
+++ src/components/invitationSection/Traffic.js
@@ -14,9 +14,14 @@ const Traffic = () => {
           <ul className={styles.traffic__list}>
             {
               trafficList.map((item, idx) => (
+                idx === 0 ? 
                 <li key={item.traffic + idx} className={styles.traffic__item}>
                   <p>{item.traffic === "" ? "버스로 오시는 길" : item.traffic}</p>
                   <p>{item.content === "" ? "남울산우체국 하차(도보2분) : 427, 922, 925, 975" : item.content}</p>
+                </li> : 
+                <li key={item.traffic + idx} className={styles.traffic__item}>
+                  <p>{item.traffic === "" ? "지하철로 오시는 길" : item.traffic}</p>
+                  <p>{item.content === "" ? "태화강역 하차 (동해선)" : item.content}</p>
                 </li>
               ))
             }
