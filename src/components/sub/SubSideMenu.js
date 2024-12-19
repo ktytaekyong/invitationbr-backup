06893c0533dae077b16de24e017d8701cb8f57f9
hejo47 <hejo_47@naver.com>
2024-12-19 17:51:21 +0900

    feat: 서브 페이지 타이틀 작업 중(제작 관리)_241219

---
+++ src/components/sub/SubSideMenu.js
@@ -0,0 +1,18 @@
+/* import */
+import { useContext, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+/* CSS Module */
+import styles from "../../css/module/sub/SubTitle.module.scss";
+/* Context */
+import { SetContext } from "../../store/option-set-context.js";
+/* Image */
+import logoImg from "../../img/main/logo.svg";
+import myInfo from "../../img/icon/icon_info.png";
+
+const SubSideMenu = () => {
+  return (
+    <div>SubSideMenu</div>
+  )
+}
+
+export default SubSideMenu
