7ea246dc3e90e25abf44f992bc20c3e6dcf13c14
kimtaekyong <kty@boram.com>
2024-12-30 10:58:54 +0900

    Git build img

--- src/components/sub/ManageItem.js
+++ src/components/sub/ManageItem.js
@@ -1,9 +1,9 @@
 /* import */
 import { useContext, useState, useEffect } from "react";
-import { Link, useNavigate  } from "react-router-dom";
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { DateConverterDot, DDayCalculator, AddDaysToDate } from "../../utils/helpers.js"
+import { Link, useNavigate } from "react-router-dom";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { DateConverterDot, DDayCalculator, AddDaysToDate } from "../../utils/helpers.js";
 /* Component */
 import Button from "../layout/Button.js";
 import ButtonWrapper from "../layout/ButtonWrapper.js";
@@ -14,7 +14,7 @@ import { SetContext } from "../../store/option-set-context.js";
 /* Image */
 import rightArrowImg from "../../img/icon/icon_arrow_right.png";
 
-const label = { inputProps: { 'aria-label': 'Switch use' } };
+const label = { inputProps: { "aria-label": "Switch use" } };
 const ManageItem = ({ inviteIdx, inviteItem, onChange, children }) => {
   const { isMobile } = useContext(SetContext);
   const navigate = useNavigate();
@@ -27,16 +27,14 @@ const ManageItem = ({ inviteIdx, inviteItem, onChange, children }) => {
             <FormControlLabel
               value={inviteItem.use}
               control={
-                <Switch 
-                  defaultChecked={inviteItem.use} 
-                  checked={inviteItem.use} 
-                  onChange={() => (
-                    onChange((prev) => (
-                      prev.map((item, idx) => (
-                        inviteIdx === idx ? {...item, use: !item.use} : item
-                      ))
-                    ))
-                  )} 
+                <Switch
+                  defaultChecked={inviteItem.use}
+                  checked={inviteItem.use}
+                  onChange={() =>
+                    onChange((prev) =>
+                      prev.map((item, idx) => (inviteIdx === idx ? { ...item, use: !item.use } : item))
+                    )
+                  }
                 />
               }
               label={inviteItem.use ? "청첩장 사용중" : "청첩장 미사용"}
@@ -48,37 +46,43 @@ const ManageItem = ({ inviteIdx, inviteItem, onChange, children }) => {
         <p className={styles.manage__item_title}>{inviteItem.title}</p>
         <div className={styles.manage__item_date}>
           <div className={styles.manage__item_date_wrap}>
-            <p className={styles.manage__item_date_delete}>{Number(DDayCalculator(inviteItem.weddingDate)) + 30}일 남음</p>
-            <p className={styles.manage__item_date_day}>{DateConverterDot(AddDaysToDate(inviteItem.weddingDate, 30))}까지</p>
+            <p className={styles.manage__item_date_delete}>
+              {Number(DDayCalculator(inviteItem.weddingDate)) + 30}일 남음
+            </p>
+            <p className={styles.manage__item_date_day}>
+              {DateConverterDot(AddDaysToDate(inviteItem.weddingDate, 30))}까지
+            </p>
           </div>
-          <Link to="/Produce/Preview"><span>청첩장 보기</span><img src={rightArrowImg} alt="" /></Link>
+          <Link to="/Produce/Preview">
+            <span>청첩장 보기</span>
+            <img src={rightArrowImg} alt="" />
+          </Link>
         </div>
         <div className={styles.manage__item_content}>
           <div className={styles.manage__item_content_photo}>
-            <img src="/img/intro/intro_photo1.png" alt="" />
+            <img src="./img/intro/intro_photo1.png" alt="" />
           </div>
           <div className={styles.manage__item_content_info}>
-            <p>최초 작성일<span>{inviteItem.createDate}</span></p>
-            <p>최종 수정일<span>{inviteItem.modifyDate}</span></p>
-            <p>삭제 예정일<span>{(AddDaysToDate(inviteItem.weddingDate, 30))}</span></p>
+            <p>
+              최초 작성일<span>{inviteItem.createDate}</span>
+            </p>
+            <p>
+              최종 수정일<span>{inviteItem.modifyDate}</span>
+            </p>
+            <p>
+              삭제 예정일<span>{AddDaysToDate(inviteItem.weddingDate, 30)}</span>
+            </p>
             <Button content="참석 여부 확인" styleType="manage" onClick={() => navigate("/Manager/Detail")}></Button>
           </div>
         </div>
         <ButtonWrapper>
-          <Button
-            content="복사"
-            styleType="manage__cancel"
-          ></Button>
-          <Button 
-            content="청첩장 편집하기" 
-            onClick={() => navigate("/Produce")}
-            styleType="manage__point"
-          />
+          <Button content="복사" styleType="manage__cancel"></Button>
+          <Button content="청첩장 편집하기" onClick={() => navigate("/Produce")} styleType="manage__point" />
         </ButtonWrapper>
       </div>
       <Button content="카카오톡 공유하기" styleType="manage__kakao"></Button>
     </li>
-  )
-}
+  );
+};
 
 export default ManageItem;
